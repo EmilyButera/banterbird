ffd55d260c96ec8caa11238b6bd5d744f05e935f
Emily <147433015+EmilyButera@users.noreply.github.com>
2025-04-18 09:00:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import json
 
 app = Flask(__name__)
@@ -11,6 +11,16 @@ def index():
 def get_posts():
     with(open("data.json", "r")) as file:
         post = json.load(file)
+
+@app.route('/api/posts', methods=['POST'])
+def add_post():
+    new_post = request.get_json()
+    with(open("data.json", "r")) as file:
+        post = json.load(file)
+        post.insert(0, new_post)
+    with(open("data.json", "r")) as file:
+        json.dump(posts, file, indent=4)
+    return jsonify({"status": "sucess"}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
